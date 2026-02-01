ef30cdc2277714976d3071c2ee4290dc84f247d6
Minhajul43 <minhajul255685islam@gmail.com>
2026-02-01 22:23:15 +0600

    convert problem

---
+++ convert.js
@@ -0,0 +1,13 @@
+function feetToInch(inch) {
+  let feet = inch / 12;
+  let feetNumber = parseInt(feet);
+  let Fraction = inch % 12;
+
+  let result = feetNumber+' ft and '+ Fraction+' inch.';
+
+  console.log(result);
+}
+
+const heith = 75;
+feetToInch(heith);
+// console.log(feetToInch(heith));
